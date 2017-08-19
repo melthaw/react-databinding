5969b55d518ef9b20c5cafc85c07fd73584565b5
dz <melthaw@gmail.com>
2017-08-19 23:29:43 +0800

    fixed: package.json

---
+++ src/immutable/bind.js
@@ -0,0 +1,64 @@
+import F from './f';
+import { fromJS } from 'immutable';
+
+/**
+ *
+ * @param ctx this ref of current component
+ */
+const doChange = (ctx, path) => (value)=> {
+	if (path == null || path.trim() === '') {
+		return;
+	}
+
+	let propChain = path.split('.');
+	let firstProp = propChain[0];
+	if (propChain.length === 1) {
+		let next = {};
+		next[firstProp] = value;
+		ctx.setState(next);
+		return;
+	}
+
+	let next = {};
+	next[firstProp] = ctx.state[firstProp].setIn(propChain.slice(1), value);
+	ctx.setState(next);
+}
+
+/**
+ * Usage:
+ *
+ * <code>
+ *      import {oneWayBind} from 'react-redux-data-binding';
+ *      let $ = oneWayBind(this.props);
+ *      <MyComponent $("username") />
+ * </code>
+ *
+ *
+ * @param context
+ */
+export const oneWayBind = (context) => (path, defaultValue) => {
+	return F.of(fromJS(context)).at(path).value(defaultValue);
+}
+
+/**
+ * Usage :
+ * <code>
+ *    import {twoWayBind} from 'react-redux-data-binding';
+ *    let $$ = twoWayBind(this);
+ *    <MyComponent ...$$("username") />
+ * </code>
+ *
+ * @param context
+ */
+export const twoWayBind = (value, event) => (context, onChange) => (path, defaultValue) => {
+	let result = {};
+	result[value] = F.of(fromJS(context.state)).at(path).value(defaultValue);
+	result[event] = value => {
+		if (onChange != null) {
+			onChange({path, value});
+			return;
+		}
+		doChange(context, path)(value)
+	};
+	return result;
+}
